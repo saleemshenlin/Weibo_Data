ef6a92bbde6e589ae6209d954b6d855a8828ff2c
沈霖 <saleemshenlin@gmail.com>
2014-04-11 13:58:39 +0800

    开始一个新solution

---
+++ weibo_data/weibo_data/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="weibo_data.MvcApplication" Language="C#" %>
---
+++ weibo_data/weibo_data/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(weibo_data.Startup))]
+namespace weibo_data
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
