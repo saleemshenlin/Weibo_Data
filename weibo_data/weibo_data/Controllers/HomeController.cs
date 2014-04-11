805bbdf7b4dd37abe5794b53719f8c6f879dc78c
沈霖 <saleemshenlin@gmail.com>
2014-04-11 17:02:54 +0800

    增加读取文件夹里的文件，增加json.net读取json

--- weibo_data/weibo_data/Controllers/HomeController.cs
+++ weibo_data/weibo_data/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using weibo_data.Models;
 
 namespace weibo_data.Controllers
 {
@@ -10,6 +15,8 @@ namespace weibo_data.Controllers
     {
         public ActionResult Index()
         {
+            //ViewBag.Message = ReadFolder().ToString();
+            ReadFolder();
             return View();
         }
 
@@ -25,6 +32,56 @@ namespace weibo_data.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        /// <summary>
+        /// 读取文件夹中的文件名
+        /// </summary>
+        private void ReadFolder()
+        {
+            List<string> result = new List<string>();
+            string url = Server.MapPath("data");
+            DirectoryInfo TheFolder = new DirectoryInfo(url);
+            if (TheFolder != null)
+            {
+                FileInfo[] Folders = TheFolder.GetFiles();
+                foreach (FileInfo fileinfo in Folders)
+                {
+                    result.Add(fileinfo.Name);
+                }
+            }
+            ReadJsonFromFile(result);
+        }
+
+        private void ReadJsonFromFile(List<string> files)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            foreach (string filename in files)
+            {
+                string fileUrl = Server.MapPath("data/" + filename);
+                //读取文件
+                StreamReader sr = new StreamReader(fileUrl, System.Text.Encoding.UTF8);
+                try
+                {
+                    if (sr != null)
+                    {
+                        string stringLine = sr.ReadLine();
+                        if (stringLine != null)
+                        {
+                            string result = "";
+                            while (stringLine != null)
+                            {
+                                result = stringLine.Substring(1, stringLine.Length - 2);
+                                var p1 = JsonConvert.DeserializeObject<WeiboFromBigData>(result);
+                                stringLine = sr.ReadLine();
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    sr.Close();
+                }
+            }
         }
     }
 }
---
+++ weibo_data/weibo_data/Models/WeiboFromBigData.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace weibo_data.Models
+{
+    /// <summary>
+    /// 从BigData抓取来的weibo的实体类
+    /// </summary>
+    public class WeiboFromBigData
+    {
+        /// <summary>
+        /// //被评论的微博ID，当本条微博为AAA微博的评论微博时，该字段为AAA对应的微博ID。当本条微博为原创微博或转发微博时，该字段为空。
+        /// </summary>
+        [JsonProperty(PropertyName = "beCommentWeiboId")]
+        string CommentWeiboId { get; set; }
+
+        /// <summary>
+        /// //被转发的微博ID，当本条微博为AAA微博的转发微博时，该字段为AAA对应的微博ID。当本条微博为原创微博或评论微博时，该字段为空。当转发微博的源微博被删除时，该字段为-1。
+        /// </summary>
+        [JsonProperty(PropertyName = "beForwardWeiboId")]
+        string ForwardWeiboId { get; set; }
+        /// <summary>
+        /// //当前微博的评论数
+        /// </summary>
+        [JsonProperty(PropertyName = "commentCount")]
+        string CommentCount { get; set; }
+        /// <summary>
+        /// 微博内容
+        /// </summary>
+        [JsonProperty(PropertyName = "content")]
+        string Content { get; set; }
+        /// <summary>
+        /// //微博创建时间
+        /// </summary>
+        [JsonProperty(PropertyName = "createTime")]
+        string CreateTime { get; set; }
+        /// <summary>
+        /// //赞的数目
+        /// </summary>
+        [JsonProperty(PropertyName = "praiseCount")]
+        string PraiseCount { get; set; }
+
+        /// <summary>
+        /// //转发数
+        /// </summary>
+        [JsonProperty(PropertyName = "reportCount")]
+        string ReportCount { get; set; }
+        /// <summary>
+        /// //微博来源
+        /// </summary>
+        [JsonProperty(PropertyName = "source")]
+        string Source { get; set; }
+
+        /// <summary>
+        /// //微博创建者
+        /// </summary>
+        [JsonProperty(PropertyName = "souuserIdrce")]
+        string UserId { get; set; }
+
+        /// <summary>
+        /// //微博id
+        /// </summary>
+        [JsonProperty(PropertyName = "weiboId")]
+        string WeiboId { get; set; }
+        /// <summary>
+        /// //微博对应的url
+        /// </summary>
+        [JsonProperty(PropertyName = "weiboUrl")]
+        string WeiboUrl { get; set; }
+    }
+}
