9ef26feb570982c1cc54b64c1cce2780f14ea241
沈霖 <saleemshenlin@gmail.com>
2014-06-02 21:33:10 +0800

    增加部分poi签到数据 a

--- weibo_data/WeiboDataWithSDK/WeiboDataWithSDK/Controllers/HomeController.cs
+++ weibo_data/WeiboDataWithSDK/WeiboDataWithSDK/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using weibo_data.Dals;
 using WeiboDataWithSDK.Models;
 
 namespace WeiboDataWithSDK.Controllers
@@ -18,11 +19,12 @@ namespace WeiboDataWithSDK.Controllers
         public static string PASSWORD = "1qaz2wsx";
         static Client Sina = null;
         string UserID = string.Empty;
+        private WeiboBigDataContext db = new WeiboBigDataContext();
         public ActionResult Index()
         {
             ViewBag.Message = InitWeiboOAuth();
             ViewBag.User = LoadUserInfo();
-            //LoadPoiUesr();
+            LoadPoiUesr();
             return View();
         }
 
@@ -65,58 +67,103 @@ namespace WeiboDataWithSDK.Controllers
         }
         /// <summary>
         /// place/pois/users
-        /// 米亚罗 B2094757D16FAAFA4392 530
-        /// 米亚罗自然保护区 B2094653DB64ABFD499C
-        /// 
+        /// 米亚罗 B2094757D16FAAFA4392 530 31.657766465 102.80677515
+        /// 米亚罗自然保护区 B2094653DB64ABFD499C 193 31.65477 102.810272
+        /// 古尔沟温泉山庄  B2094653DB64A1FB469C 31.49766 102.97848
+        /// 毕棚沟旅游景区 B2094653DB64A0FA459E 31.40819 102.98744
+        /// 毕棚沟 B2094751D569AAFA409E 103.071091 31.402451
+        /// 娜姆湖 B2094653DB64A0FA4899 102.95245 31.31969
+        /// 桃坪羌寨 B2094757D16CAAF4469C  103.45722781 31.554634941
+        /// 桃坪寨  B2094653DB64A1F8489D 103.46205 31.57092
+        /// 白空寺 B2094653DB64ABFE459D 103.45392 31.60256
+        /// 甘堡藏家 B2094653DB64A1F4409D 103.19954 31.48252
         /// </summary>
         private void LoadPoiUesr()
         {
-
+            int times = 0;
             if (Sina == null)
             {
                 Sina = new Client(OAUTH);
             }
-            dynamic json = Sina.API.Dynamic.Place.POIUsers("B2094757D16FAAFA4392", 50, 1, false);
+            dynamic json = Sina.API.Dynamic.Place.POIUsers("B2094653DB64A1F4409D", 50, 1, false);
             //IEnumerable<NetDimension.Weibo.Entities.user.Entity> users = json;
             if (json.IsDefined("total_number"))
-            {
+              {
                 int totalNumber = int.Parse(json.total_number);
+                times = totalNumber / 50 + 1;
             }
-            if (json.IsDefined("users"))
+            LoadPoiUserReadJson(json);
+            for (int i = 2; i <= times; i++)
             {
-                foreach (var user in json.users)
-                {
-                    User userEntity = new User();
-                    userEntity.ID = user.id;
-                    userEntity.IDStr = user.idstr;
-                    userEntity.Name = user.name;
-                    userEntity.Province = user.province;
-                    userEntity.City = user.city;
-                    userEntity.Location = user.location;
-                    userEntity.ProfileImageUrl = user.profile_image_url;
-                    userEntity.Gender = user.gender;
-                    userEntity.CreatedAt = user.created_at;
-                    if (user.IsDefined("status"))
-                    {
-                        Status statusEntity = new Status();
-                        statusEntity.CreatedAt = user.status.created_at;
-                        statusEntity.ID = user.status.id;
-                        statusEntity.Text = user.status.text;
-                        statusEntity.Source = user.status.source;
-                        statusEntity.ThumbnailPictureUrl = user.status.thumbnail_pic;
-                        statusEntity.MiddleSizePictureUrl = user.status.bmiddle_pic;
-                        statusEntity.OriginalPictureUrl = user.status.original_pic;
-                        statusEntity.Long = 102.806778f;
-                        statusEntity.Lat = 31.657766f;
-                        statusEntity.RepostsCount = int.Parse(user.status.reposts_count);
-                        statusEntity.CommentsCount = int.Parse(user.status.comments_count);
-                        statusEntity.AttitudeCount = int.Parse(user.status.attitudes_count);
-                        statusEntity.User = userEntity;
-                    }
-                }
+                dynamic newjson = Sina.API.Dynamic.Place.POIUsers("B2094653DB64A1F4409D", 50, i, false);
+                LoadPoiUserReadJson(newjson);
             }
             //return users;
         }
+
+        private void LoadPoiUserReadJson(dynamic json)
+        {
+            try
+            {
+                if (json.IsDefined("users"))
+                {
+                    foreach (var user in json.users)
+                    {
+                        var isInUserDb = db.UserDb.Find(user.id);
+                        if (isInUserDb == null)
+                        {
+                            User userEntity = new User();
+                            userEntity.ID = user.id;
+                            userEntity.IDStr = user.idstr;
+                            userEntity.Name = user.name;
+                            userEntity.Province = user.province;
+                            userEntity.City = user.city;
+                            userEntity.Location = user.location;
+                            userEntity.ProfileImageUrl = user.profile_image_url;
+                            userEntity.Gender = user.gender;
+                            userEntity.CreatedAt = user.created_at;
+                            db.UserDb.Add(userEntity);
+                            db.SaveChanges();
+                            if (user.IsDefined("status"))
+                            {
+                                var isInStatusDb = db.StatusDb.Find(user.status.id);
+                                if (isInStatusDb == null)
+                                {
+                                    Status statusEntity = new Status();
+                                    statusEntity.CreatedAt = user.status.created_at;
+                                    statusEntity.ID = user.status.id;
+                                    statusEntity.Text = user.status.text;
+                                    if (statusEntity.Text.IndexOf("此微博已被作者删除") <= -1)
+                                    {
+                                        statusEntity.Source = user.status.source;
+                                        if (user.status.IsDefined("thumbnail_pic"))
+                                        {
+                                            statusEntity.ThumbnailPictureUrl = user.status.thumbnail_pic;
+                                            statusEntity.MiddleSizePictureUrl = user.status.bmiddle_pic;
+                                            statusEntity.OriginalPictureUrl = user.status.original_pic;
+                                        }
+                                        statusEntity.Long = 103.19954f;// 
+                                        statusEntity.Lat = 31.48252f;
+                                        statusEntity.RepostsCount = int.Parse(user.status.reposts_count);
+                                        statusEntity.CommentsCount = int.Parse(user.status.comments_count);
+                                        statusEntity.AttitudeCount = int.Parse(user.status.attitudes_count);
+                                        statusEntity.User = userEntity;
+                                        db.StatusDb.Add(statusEntity);
+
+                                        db.SaveChanges();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            finally
+            {
+
+            }
+        }
+
         /// <summary>
         /// 获取用户信息，我们来个直接把JSON写到页面的方法和下面的方法区别下
         /// </summary>
